8686dbb8900d9d9051240316cd6d0fac4ddba26a
daief48 <daief20103048@gmail.com>
2023-03-30 00:40:02 +0600

    Add product according to category

--- shop/views.py
+++ shop/views.py
@@ -4,10 +4,18 @@ from .models import Product
 from math import ceil
 # Create your views here.
 def index(request):
-    products= Product.objects.all()
-    n= len(products)
-    nSlides= n//4 + ceil((n/4)-(n//4))
-    allProds=[[products, range(1, len(products)), nSlides],[products, range(1, len(products)), nSlides]]
+    # products= Product.objects.all()
+    # n= len(products)
+    # nSlides= n//4 + ceil((n/4)-(n//4))
+    allProds = []
+    catprods = Product.objects.values('category','id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n= len(prod)
+        nSlides= n//4 + ceil((n/4)-(n//4))
+        allProds.append([prod,range(1,nSlides),nSlides])
+    # allProds=[[products, range(1, len(products)), nSlides],[products, range(1, len(products)), nSlides]]
     params={'allProds':allProds }
     return render(request,"shop/index.html", params)
 def about(request):
